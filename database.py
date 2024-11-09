56dc0e38332108445ef8d204ed728f739d7369cd
kazuaki-ito <kazuaki-ito@isoshi-moustache.com>
2024-11-09 15:44:56 +0900

    ビルド方法変更

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"  # 必要に応じて変更
+DATABASE_URL = "sqlite:///./db/test.db"  # 必要に応じて変更
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
